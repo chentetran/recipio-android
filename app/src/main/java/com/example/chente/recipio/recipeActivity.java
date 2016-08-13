16d7649a4f007c25afb5878c40f00b563fc687c5
Vincent Tran <vincenttran23@gmail.com>
2016-08-13 15:03:32 -0400

    voice recognition works

--- app/src/main/java/com/example/chente/recipio/recipeActivity.java
+++ app/src/main/java/com/example/chente/recipio/recipeActivity.java
@@ -2,9 +2,6 @@ package com.example.chente.recipio;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
@@ -18,15 +15,31 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
-public class recipeActivity extends AppCompatActivity {
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
 
+import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
+
+public class recipeActivity extends AppCompatActivity implements RecognitionListener {
+
+    private static final String KWS_SEARCH = "wakeup";
+
+    private static final String NEXT = "hello friend";
+    private static final String BACK = "back";
+    private static final String REPEAT = "repeat";
+
+    private String id;
     private String TAG = "tag";
     private int selectedStep = 0;
     private TextToSpeech tts;
     private String[] recipeSteps;
-    private SpeechRecognizer speechRecognizer;
+    private SpeechRecognizer recognizer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +54,34 @@ public class recipeActivity extends AppCompatActivity {
             }
         });
 
-        // init speechrecognizer
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        // init sphinx voice recognition
+//        new AsyncTask<Void, Void, Exception>() {
+//            @Override
+//            protected Exception doInBackground(Void... voids) {
+//                return null;
+//            }
+//
+//
+//        }.execute();
+                try {
+                    Assets assets = new Assets(recipeActivity.this);
+                    File assetDir = assets.syncAssets();
+                    setupRecognizer(assetDir);
+                } catch (IOException e) {
+                    Log.e("voice_init", e.toString());
+                }
 
         // get info about this recipe
         Intent intent = getIntent();
         String recipe = intent.getStringExtra("recipe");
-        String id = intent.getStringExtra("id");
+        id = intent.getStringExtra("id");
 
         setTitle(recipe);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         final String recipeUrl = "https://recipio.herokuapp.com/recipes?recipe=" + id;
         Ion.with(this)
@@ -61,10 +93,11 @@ public class recipeActivity extends AppCompatActivity {
                         if (e != null) { // error occurred
                             Toast.makeText(recipeActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
                         }
-                        
+
                         JsonArray recipeStepsJson = result.getAsJsonArray("steps");
                         int length = recipeStepsJson.size();
                         recipeSteps = new String[length];
+
                         for (int i = 0; i < length; i++) {
                             recipeSteps[i] = recipeStepsJson.get(i).toString();
                         }
@@ -79,74 +112,48 @@ public class recipeActivity extends AppCompatActivity {
                         ListView stepsList = (ListView) findViewById(R.id.stepsList);
                         stepsList.setAdapter(adapter);
 
-                        speak();
+                        tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
                     }
                 });
     }
 
-    private void speak() {
-        tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
-        selectedStep++;
+    private void setupRecognizer(File assetsDir) throws IOException {
+        recognizer = defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+
+                // Threshold to tune for keyphrase to balance between false alarms and misses
+                .setKeywordThreshold(1e-45f)
+
+                // Use context-independent phonetic search, context-dependent is too slow for mobile
+                .setBoolean("-allphone_ci", true)
+
+                .getRecognizer();
+        recognizer.addListener(this);
+
+        File commandGrammar = new File(assetsDir, "commands.gram");
+        recognizer.addKeywordSearch(KWS_SEARCH, commandGrammar);
     }
 
-    class Listener implements RecognitionListener {
-        public void onReadyForSpeech(Bundle params)
-        {
-            Log.d(TAG, "onReadyForSpeech");
-        }
-        public void onBeginningOfSpeech()
-        {
-            Log.d(TAG, "onBeginningOfSpeech");
-        }
-        public void onRmsChanged(float rmsdB)
-        {
-            Log.d(TAG, "onRmsChanged");
-        }
-        public void onBufferReceived(byte[] buffer)
-        {
-            Log.d(TAG, "onBufferReceived");
-        }
-        public void onEndOfSpeech()
-        {
-            Log.d(TAG, "onEndofSpeech");
-        }
-        public void onError(int error)
-        {
-            Log.d(TAG,  "error " +  error);
-        }
-        public void onResults(Bundle results)
-        {
-            String str = new String();
-            Log.d(TAG, "onResults " + results);
-            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            for (int i = 0; i < data.size(); i++)
-            {
-                Log.d(TAG, "result " + data.get(i));
-                str += data.get(i);
-            }
-            Log.d("ay", str);
-        }
-        public void onPartialResults(Bundle partialResults)
-        {
-            Log.d(TAG, "onPartialResults");
-        }
-        public void onEvent(int eventType, Bundle params)
-        {
-            Log.d(TAG, "onEvent " + eventType);
-        }
+    private void listen(String searchName) {
+        recognizer.stop();
+
+        recognizer.startListening(searchName);
+
+    }
+    
+
+    private void speak() {
+        recognizer.stop();
+        tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+        recognizer.startListening(KWS_SEARCH);
     }
 
     class ttsUtteranceListener extends UtteranceProgressListener {
         @Override
         public void onDone(String utteranceId) {
             // Done speaking, so call speech recognizer
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            //... put other settings in the Intent
-            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 60000);
-            startActivityForResult(intent, 123);
-
+            listen(KWS_SEARCH);
         }
 
         @Override
@@ -156,7 +163,92 @@ public class recipeActivity extends AppCompatActivity {
 
         @Override
         public void onStart(String utteranceId) {
+            recognizer.stop();
         }
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.d("end", "END OF SPEECH");
+        if (!recognizer.getSearchName().equals(KWS_SEARCH))
+            listen(KWS_SEARCH);
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis != null) {
+            String text = hypothesis.getHypstr();
+            text = text.replaceAll("\\s","");
+
+
+            if (text.equals(NEXT)) {
+                recognizer.stop();
+            }
+            else if (text.equals(BACK)) {
+                Toast.makeText(recipeActivity.this, "back", Toast.LENGTH_SHORT).show();
+                Log.d("back", String.valueOf(selectedStep));
+
+                selectedStep--;
+                tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+
+            }
+            else if (text.equals(REPEAT)) {
+                Toast.makeText(recipeActivity.this, "repeat", Toast.LENGTH_SHORT).show();
+                Log.d("repeat", String.valueOf(selectedStep));
+
+                tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+
+            }
+        }
+    }
+
+    @Override
+    public void onError(Exception e) {
+        
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        if (text.equals(NEXT)) {
+            Toast.makeText(recipeActivity.this, "next", Toast.LENGTH_SHORT).show();
+            Log.d("next", String.valueOf(selectedStep));
+            selectedStep++;
+            tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+
+
+        }
+        else if (text.equals(BACK)) {
+            Toast.makeText(recipeActivity.this, "back", Toast.LENGTH_SHORT).show();
+            Log.d("back", String.valueOf(selectedStep));
+
+            selectedStep--;
+            tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+
+        }
+        else if (text.equals(REPEAT)) {
+            Toast.makeText(recipeActivity.this, "repeat", Toast.LENGTH_SHORT).show();
+            Log.d("repeat", String.valueOf(selectedStep));
+
+            tts.speak(recipeSteps[selectedStep], TextToSpeech.QUEUE_FLUSH, null, "TODO??");
+
+        }
+    }
+
+    @Override
+    public void onTimeout() {
+        listen(KWS_SEARCH);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        recognizer.cancel();
+        recognizer.shutdown();
     }
 }
 
